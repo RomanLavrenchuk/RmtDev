ba7624bfc833f56debf971e864838e33cccafa6f
Roman <polskaago@gmail.com>
2024-11-27 23:34:04 -0500

    Child Pattern

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,12 @@
-import JobList from "./JobList";
-import PaginationControls from "./PaginationControls";
-import ResultsCount from "./ResultsCount";
-import SortingControls from "./SortingControls";
 
-export default function Sidebar({jobItems}) {
+export default function Sidebar({children}:{children: React.ReactNode}) {
   return (
     <div className="sidebar">
-      <div className="sidebar__top">
-        <ResultsCount />
-        <SortingControls />
-      </div>
-      <JobList jobItems={jobItems}/>
-      <PaginationControls />
+      {children}
     </div>
   );
+}
+
+export function SidebarTop({children}:{children: React.ReactNode}){
+  return <div className="sidebar__top">{children}</div>
 }
